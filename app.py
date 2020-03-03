f62fe433f336168762e6b1f44242d5db5986e725
mlbiche <tmlb@protonmail.com>
2020-03-03 11:34:34 +0100

    Add config.json file loading
    - Add config.json file loading
    - Use config for application URL
    - Fix single quote to double quote

--- app.py
+++ app.py
@@ -2,13 +2,17 @@ import subprocess
 from starlette.applications import Starlette
 from starlette.requests import Request
 from starlette.responses import FileResponse
+import ujson
+
+with open("config.json", "r") as configData:
+    config: dict = ujson.load(configData)
 
 app: Starlette = Starlette(debug=True)
 
 @app.route("/pdf/{receiptContentBase64}/{name}")
 async def downloadReceipt(request: Request):
-    receiptURL: str = f"http://app.aposto.ch/receipt/receipt.html?receiptContent={request.path_params['receiptContentBase64']}"
+    receiptURL: str = f"{config['apostoAppURL']}/receipt/receipt.html?receiptContent={request.path_params['receiptContentBase64']}"
 
-    subprocess.run(['npx', 'electron-pdf', receiptURL, 'out.pdf'])
+    subprocess.run(["npx", "electron-pdf", receiptURL, "out.pdf"])
 
-    return FileResponse('out.pdf')
+    return FileResponse("out.pdf")
