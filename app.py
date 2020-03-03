e4c432c733774b4cf8fb452b34e26dc1517ae8ff
Mlbiche <tmlb@protonmail.com>
2020-03-03 15:45:49 +0100

    Move API key to Python .env
    - Move API key to Python .env
    - Version config.json
    - Fix variable naming
    - Add formatter and linter
    - Format app.py
    - Use proper ujson function names

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ import urllib.parse
 from time import time
 from datetime import datetime
 import requests
+from dotenv import load_dotenv
+import os
+
 from requests import Response
 from starlette.applications import Starlette
 from starlette.requests import Request
@@ -11,63 +14,70 @@ from starlette.responses import FileResponse, UJSONResponse
 from starlette.status import HTTP_400_BAD_REQUEST
 import ujson
 
+load_dotenv()
+SEND_IN_BLUE_API_KEY = os.getenv("SEND_IN_BLUE_API_KEY")
+
 with open("config.json", "r") as configData:
     config: dict = ujson.load(configData)
 
 app: Starlette = Starlette(debug=True)
 
+
 @app.route("/pdf/{receiptContentBase64}/{name}")
 async def downloadReceipt(request: Request):
-    receiptURL: str = f"{config['apostoAppURL']}/receipt/receipt.html?receiptContent={request.path_params['receiptContentBase64']}"
+    receipt_url: str = f"{config['apostoAppURL']}/receipt/receipt.html?receiptContent={request.path_params['receiptContentBase64']}"
 
-    subprocess.run(["npx", "electron-pdf", receiptURL, "out.pdf"])
+    subprocess.run(["npx", "electron-pdf", receipt_url, "out.pdf"])
 
     return FileResponse("out.pdf")
 
+
 @app.route("/email/{receiptContentBase64}")
 async def emailReceipt(request: Request):
-    receiptContent: dict = ujson.decode(base64.b64decode(request.path_params["receiptContentBase64"]))
-    receiptURL: str = urllib.parse.quote(f"{config['apostoAppURL']}/receipt/receipt.html?receiptContent={request.path_params['receiptContentBase64']}", safe='')
-    receiptFilename: str = f"facture-{round(time())}.pdf"
-    url: str = f"{config['apostoAPIURL']}/pdf/{receiptURL}/{receiptFilename}"
+    receipt_content: dict = ujson.loads(
+        base64.b64decode(request.path_params["receiptContentBase64"])
+    )
+    receipt_url: str = urllib.parse.quote(
+        f"{config['apostoAppURL']}/receipt/receipt.html?receiptContent={request.path_params['receiptContentBase64']}",
+        safe="",
+    )
+    receipt_filename: str = f"facture-{round(time())}.pdf"
+    url: str = f"{config['apostoAPIURL']}/pdf/{receipt_url}/{receipt_filename}"
 
-    data: str = ujson.encode({
-        "sender": {
-            "email": "facture@app.aposto.ch",
-            "name": "Aposto"
-        },
-        "to": [{
-            "email": receiptContent["customer"]["email"],
-            "name": f"{receiptContent['customer']['firstName']} {receiptContent['customer']['lastName']}"
-        }],
-        "bcc": [{
-            "email": receiptContent["author"]["email"],
-            "name": receiptContent["author"]["name"]
-        }],
-        "htmlContent": f"<h1>Votre facture</h1><p>Bonjour {receiptContent['customer']['firstName']} {receiptContent['customer']['lastName']},</p><p>Vous pouvez dès à présent consulter votre facture du {datetime.now().strftime('%d/%m/%Y')} en pièce jointe.</p><p>À très bientôt,<br>{receiptContent['author']['name']}</p>",
-        "subject": "Aposto - Votre nouvelle facture",
-        "attachment": [{
-            "url": url,
-            "name": receiptFilename
-        }]
-    })
+    data: str = ujson.dumps(
+        {
+            "sender": {"email": "facture@app.aposto.ch", "name": "Aposto"},
+            "to": [
+                {
+                    "email": receipt_content["customer"]["email"],
+                    "name": f"{receipt_content['customer']['firstName']} {receipt_content['customer']['lastName']}",
+                }
+            ],
+            "bcc": [
+                {
+                    "email": receipt_content["author"]["email"],
+                    "name": receipt_content["author"]["name"],
+                }
+            ],
+            "htmlContent": f"<h1>Votre facture</h1><p>Bonjour {receipt_content['customer']['firstName']} {receipt_content['customer']['lastName']},</p><p>Vous pouvez dès à présent consulter votre facture du {datetime.now().strftime('%d/%m/%Y')} en pièce jointe.</p><p>À très bientôt,<br>{receipt_content['author']['name']}</p>",
+            "subject": "Aposto - Votre nouvelle facture",
+            "attachment": [{"url": url, "name": receipt_filename}],
+        }
+    )
 
     headers = {
         "accept": "application/json",
         "content-type": "application/json",
-        "api-key": config["sendInBlueConfig"]["APIKey"]
+        "api-key": SEND_IN_BLUE_API_KEY,
     }
-    
+
     response: Response = requests.request(
-        "POST",
-        f"{config['sendInBlueConfig']['APIURL']}/smtp/email",
-        data=data,
-        headers=headers
+        "POST", f"{config['sendInBlueAPIURL']}/smtp/email", data=data, headers=headers,
     )
 
     if response.status_code == 201:
         return UJSONResponse()
     else:
-        responseContent: dict = ujson.decode(response.text)
+        response_content: dict = ujson.loads(response.text)
 
-        return UJSONResponse(responseContent, status_code=HTTP_400_BAD_REQUEST)
+        return UJSONResponse(response_content, status_code=HTTP_400_BAD_REQUEST)
