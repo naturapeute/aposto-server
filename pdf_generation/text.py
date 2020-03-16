cfde086716f8d66965433fbf9409fcf257eba342
mlbiche <tmlb@protonmail.com>
2020-03-16 17:21:04 +0100

    Update Text class constructor to directly accept dictionnaries

--- pdf_generation/text.py
+++ pdf_generation/text.py
@@ -5,11 +5,11 @@ import pdf_generation
 from pdf_generation.text_style import TextStyle
 
 class Text:
-    def __init__(self, text: str, left: float, top: float, text_style: str):
-        self.text: str = text
-        self.left: float = left
-        self.top: float = top
-        self.text_style: TextStyle = getattr(pdf_generation.text_style, text_style)()
+    def __init__(self, dict_text: Dict):
+        self.text: str = dict_text["text"]
+        self._left: float = dict_text["left"]
+        self._top: float = dict_text["top"]
+        self.style: TextStyle = getattr(pdf_generation.text_style, dict_text["style"])()
 
     @staticmethod
     def from_dict(dict_text: Dict) -> tuple:
