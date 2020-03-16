27ea1a63bdd309fdf14903ce12228856cacc140b
Mlbiche <tmlb@protonmail.com>
2020-03-16 17:19:56 +0100

    Implement new text styles

--- pdf_generation/text_style.py
+++ pdf_generation/text_style.py
@@ -21,20 +21,50 @@ class TextStyle:
 
 
 class TitleTextStyle(TextStyle):
-    def __init__(self, align: str = ""):
-        super().__init__("Arial B", 16, align=align)
+    def __init__(self):
+        super().__init__("Arial B", 16)
 
 
 class DescriptorTextStyle(TextStyle):
-    def __init__(self, align: str = ""):
-        super().__init__("Arial B", 7, align=align)
+    def __init__(self):
+        super().__init__("Arial", 7)
+
+
+class DescriptorRightTextStyle(DescriptorTextStyle):
+    def __init__(self):
+        super().__init__()
+        self.align = "R"
+
+
+class DescriptorCenteredTextStyle(DescriptorTextStyle):
+    def __init__(self):
+        super().__init__()
+        self.align = "C"
+
+
+class BoldDescriptorTextStyle(DescriptorTextStyle):
+    def __init__(self):
+        super().__init__()
+        self.family = "Arial B"
+
+
+class BoldDescriptorRightTextStyle(BoldDescriptorTextStyle):
+    def __init__(self):
+        super().__init__()
+        self.align = "R"
+
+
+class BoldDescriptorCenteredTextStyle(BoldDescriptorTextStyle):
+    def __init__(self):
+        super().__init__()
+        self.align = "C"
 
 
 class ContentTextStyle(TextStyle):
-    def __init__(self, align: str = ""):
-        super().__init__(align=align)
+    def __init__(self):
+        super().__init__()
 
 
 class ESRCodeTextStyle(TextStyle):
-    def __init__(self, align: str = ""):
-        super().__init__("OCRB", 10, align=align)
+    def __init__(self):
+        super().__init__("OCRB", 10)
