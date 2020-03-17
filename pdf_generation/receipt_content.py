1cd9d8bd58f19b88aa3ac33f312a20e3322252c3
Mlbiche <tmlb@protonmail.com>
2020-03-17 15:54:25 +0100

    Add service list to receipt content

--- pdf_generation/receipt_content.py
+++ pdf_generation/receipt_content.py
@@ -1,6 +1,8 @@
 from typing import Dict, List
 from datetime import datetime, timezone
 
+from pdf_generation.service_codes import SERVICE_CODES
+
 
 class ReceiptContent:
     def __init__(self, receipt_content_dict: Dict):
@@ -12,6 +14,10 @@ class ReceiptContent:
         self.therapist: Therapist = Therapist(self._receipt_content_dict["therapist"])
         self.patient: Patient = Patient(self._receipt_content_dict["patient"])
         self.init_therapy_dates()
+        self.services: ServiceList = ServiceList(
+            self._receipt_content_dict["services"],
+            self._receipt_content_dict["servicePrice"],
+        )
 
     @property
     def timestamp(self) -> str:
@@ -121,3 +127,52 @@ class Patient:
         return ReceiptContent.timestamp_to_datetime(
             self._patient_dict["birthdate"]
         ).strftime("%d.%m.%Y")
+
+
+class ServiceList:
+    def __init__(self, services_dict: Dict, service_price: float):
+        self.service_price: float = service_price / 12
+        self.services: List[Service] = list(
+            Service(service, self.service_price) for service in services_dict
+        )
+
+
+class Service:
+    def __init__(self, service_dict: Dict, service_price: float):
+        self._service_dict: Dict = service_dict
+        self._service_price: float = service_price
+        self._quantity: float = self._service_dict["duration"] / 5
+
+    @property
+    def date(self) -> str:
+        return ReceiptContent.timestamp_to_datetime(
+            self._service_dict["date"]
+        ).strftime("%d.%m.%Y")
+
+    @property
+    def tarif_number(self) -> str:
+        return "590"
+
+    @property
+    def code(self) -> str:
+        return str(self._service_dict["code"])
+
+    @property
+    def quantity(self) -> str:
+        return "%.2f" % self._quantity
+
+    @property
+    def price(self) -> str:
+        return "%.2f" % self._service_price
+
+    @property
+    def amount(self) -> str:
+        return "%.2f" % (self._quantity * self._service_price)
+
+    @property
+    def code_label(self) -> str:
+        return next(
+            service_code["label"]
+            for service_code in SERVICE_CODES
+            if service_code["value"] == self._service_dict["code"]
+        )
