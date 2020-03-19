b48b3ba85996bb35c413b032b9deae87709b02fd
Mlbiche <tmlb@protonmail.com>
2020-03-19 12:50:27 +0100

    Plug ApostoPDF to API receipt regeneration
    - Plug ApostoPDF to API receipt regeneration
    - Improve typing in app.py

--- app.py
+++ app.py
@@ -9,6 +9,10 @@ import os
 from typing import List, Dict
 from pystrich.datamatrix import DataMatrixEncoder
 from pathlib import Path
+import ujson
+
+from pdf_generation.aposto_pdf import ApostoCanvas
+from pdf_generation.receipt_content import ReceiptContent
 
 from starlette.applications import Starlette
 from starlette.requests import Request
@@ -16,15 +20,14 @@ from starlette.responses import RedirectResponse, FileResponse, UJSONResponse
 from starlette.status import HTTP_400_BAD_REQUEST
 from starlette.middleware import Middleware
 from starlette.middleware.cors import CORSMiddleware
-import ujson
 
 load_dotenv()
 SEND_IN_BLUE_API_KEY = os.getenv("SEND_IN_BLUE_API_KEY")
 
 with open("config.json", "r") as configData:
     ENV = os.getenv("ENV")
-    fullConfig: Dict = ujson.load(configData)
-    config: Dict = fullConfig["PROD"] if ENV == "PROD" else fullConfig["DEV"]
+    fullConfig: Dict[str, Dict[str, str]] = ujson.load(configData)
+    config: Dict[str, str] = fullConfig["PROD"] if ENV == "PROD" else fullConfig["DEV"]
 
 middleware: List[Middleware] = [
     Middleware(CORSMiddleware, allow_origins=[config["apostoAppURL"]])
@@ -35,7 +38,11 @@ app: Starlette = Starlette(debug=True, middleware=middleware)
 
 @app.route("/pdf/{receipt_content_base_64}/{name}")
 async def downloadReceipt(request: Request):
-    receipt_path: Path = generateReceipt(request.path_params["receipt_content_base_64"])
+    receipt_path: Path = generateReceipt(
+        ReceiptContent(
+            parseReceiptContent(request.path_params["receipt_content_base_64"])
+        )
+    )
 
     return FileResponse(receipt_path.as_posix())
 
@@ -46,7 +53,7 @@ async def emailReceipt(request: Request):
         request.path_params["receipt_content_base_64"]
     )
 
-    receipt_path: Path = generateReceipt(request.path_params["receipt_content_base_64"])
+    receipt_path: Path = generateReceipt(ReceiptContent(receipt_content))
 
     with open(receipt_path.as_posix(), "rb") as receipt_file:
         receipt_file_base_64 = base64.b64encode(receipt_file.read())
@@ -74,7 +81,7 @@ async def emailReceipt(request: Request):
         }
     )
 
-    headers: Dict = {
+    headers: Dict[str, str] = {
         "accept": "application/json",
         "content-type": "application/json",
         "api-key": SEND_IN_BLUE_API_KEY,
@@ -87,21 +94,9 @@ async def emailReceipt(request: Request):
     if response.status_code == 201:
         return UJSONResponse()
 
-    response_content: Dict = ujson.loads(response.text)
+    response_content: Dict[str, str] = ujson.loads(response.text)
 
     return UJSONResponse(response_content, status_code=HTTP_400_BAD_REQUEST)
-
-
-@app.route("/datamatrix/{datamatrix_str}/datamatrix.png")
-async def dataMatrix(request: Request):
-    decoded_data_matrix_st: str = base64.b64decode(
-        request.path_params["datamatrix_str"]
-    ).decode()
-    encoder: DataMatrixEncoder = DataMatrixEncoder(decoded_data_matrix_st)
-
-    encoder.save("datamatrix.png")
-
-    return FileResponse("datamatrix.png")
 
 
 @app.route("/favicon.ico")
@@ -109,26 +104,44 @@ async def icon(request: Request):
     return RedirectResponse("https://terrapeute.ch/img/favicon.png")
 
 
-def generateReceipt(receipt_content_base_64: str) -> Path:
-    receipt_content = parseReceiptContent(receipt_content_base_64)
-    receipt_url: str = f"{config['apostoAppURL']}/receipt/receipt.html?receiptContent={receipt_content_base_64}"
-
+def generateReceipt(receipt_content: ReceiptContent) -> Path:
     receipt_dir: Path = Path(
-        f"./out/{receipt_content['author']['name']}/{receipt_content['author']['RCCNumber']}"
+        f"./out/{receipt_content.author.name}/{receipt_content.author.RCC}"
     )
     receipt_dir.mkdir(parents=True, exist_ok=True)
     receipt_path: Path = receipt_dir.joinpath(
-        f"invoice-{receipt_content['timestamp']}.pdf"
+        f"invoice-{receipt_content.timestamp}.pdf"
     )
 
     if not receipt_path.exists():
-        sanitized_receipt_path = receipt_path.as_posix().replace(" ", r"\ ")
-        my_env = os.environ.copy()
-        out, err = subprocess.Popen(
-            f"npx electron-pdf {receipt_url} {sanitized_receipt_path}", shell=True, env=my_env, stdout=subprocess.PIPE, stderr=subprocess.PIPE
-        ).communicate()
-        print(out)
-        print(err)
+        cvs: ApostoCanvas = ApostoCanvas(receipt_path.as_posix())
+
+        cvs.draw_full_receipt(
+            [
+                Path("pdf_generation/templates/descriptor_templates/header_template.json"),
+                Path("pdf_generation/templates/descriptor_templates/author_template.json"),
+                Path("pdf_generation/templates/descriptor_templates/patient_template.json"),
+                Path(
+                    "pdf_generation/templates/descriptor_templates/other_fields_template.json"
+                ),
+                Path(
+                    "pdf_generation/templates/descriptor_templates/services_template.json"
+                ),
+                Path("pdf_generation/templates/descriptor_templates/footer_template.json"),
+            ],
+            [Path("pdf_generation/templates/graphic_templates/frame_template.json")],
+            [
+                Path("pdf_generation/templates/value_templates/author_template.json"),
+                Path("pdf_generation/templates/value_templates/patient_template.json"),
+                Path("pdf_generation/templates/value_templates/other_fields_template.json"),
+                Path("pdf_generation/templates/value_templates/services_template.json"),
+                Path("pdf_generation/templates/value_templates/footer_template.json"),
+            ],
+            receipt_content,
+        )
+
+        cvs.showPage()
+        cvs.save()
 
     return receipt_path
 
