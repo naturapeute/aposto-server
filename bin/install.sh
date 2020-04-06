c5076cec957bff9e673a25f1a8c4ad64444c4c4a
Mlbiche <tmlb@protonmail.com>
2020-04-06 09:45:39 +0200

    Remove all NPM dependencies

--- bin/install.sh
+++ bin/install.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-if [ -s "$HOME/.nvm/nvm.sh" ]; then
-    source "$HOME/.nvm/nvm.sh"
-fi
-
-npm install
 python3 -m venv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
--- bin/start.sh
+++ bin/start.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
 source venv/bin/activate
-export DISPLAY=':99.0'
-Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1 &
-
-if [ -s "$HOME/.nvm/nvm.sh" ]; then
-    source "$HOME/.nvm/nvm.sh"
-fi
 
 uvicorn --host 0.0.0.0 --port 8080 --workers 4 app:app
