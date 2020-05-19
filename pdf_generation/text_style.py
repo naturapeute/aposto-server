ea9e7b62b35b81931280fce9dd6e7e0d04f7dfe4
Mlbiche <tmlb@protonmail.com>
2020-05-19 15:55:20 +0200

    Implement QR-invoice text styles

--- pdf_generation/text_style.py
+++ pdf_generation/text_style.py
@@ -112,3 +112,28 @@ class SmallBoldValueTextStyle(TextStyle):
 class ESRCodeTextStyle(TextStyle):
     def __init__(self):
         super().__init__("OCRB", 10)
+
+
+class QRInvoiceMainTitleTextStyle(TextStyle):
+    def __init__(self):
+        super().__init__("Arial B", 11)
+
+
+class QRInvoicePaymentSectionTextStyle(TextStyle):
+    def __init__(self):
+        super().__init__(size=10)
+
+
+class QRInvoicePaymentSectionHeaderTextStyle(QRInvoiceMainTitleTextStyle):
+    def __init__(self):
+        super().__init__(size=8)
+
+
+class QRInvoiceReceiptTextStyle(TextStyle):
+    def __init__(self):
+        super().__init__(size=8)
+
+
+class QRInvoiceReceiptHeaderTextStyle(QRInvoiceMainTitleTextStyle):
+    def __init__(self):
+        super().__init__(size=6)
