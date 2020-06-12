e0118696f9cc1c0e7cc9791b8653d3a21535a74c
Mlbiche <tmlb@protonmail.com>
2020-06-12 11:30:47 +0200

    Add OpenAPI documentation

--- app.py
+++ app.py
@@ -6,16 +6,18 @@ from typing import Dict, List
 
 import requests
 import ujson
+import yaml
 from dotenv import load_dotenv
 from pydantic import ValidationError
 from requests import Response as RequestsResponse
-from spectree import Response, SpecTree
 from starlette.applications import Starlette
 from starlette.middleware import Middleware
 from starlette.middleware.cors import CORSMiddleware
 from starlette.requests import Request
 from starlette.responses import FileResponse, RedirectResponse, UJSONResponse
+from starlette.schemas import SchemaGenerator
 from starlette.status import HTTP_400_BAD_REQUEST
+from swagger_ui import api_doc
 
 from models.invoice import Invoice
 from pdf_generation.aposto_pdf import ApostoCanvas
@@ -29,7 +31,60 @@ with open("config.json", "r") as configData:
     fullConfig: Dict[str, Dict[str, str]] = ujson.load(configData)
     config: Dict[str, str] = fullConfig["PROD"] if ENV == "PROD" else fullConfig["DEV"]
 
-api: SpecTree = SpecTree("starlette")
+invoice_schema: dict = Invoice.schema()
+definitions_schema: dict = invoice_schema["definitions"]
+invoice_schema.pop("definitions", None)
+
+schemas: SchemaGenerator = SchemaGenerator(
+    {
+        "openapi": "3.0.0",
+        "info": {"title": "Aposto API", "version": "1.0"},
+        "servers": [],
+        "components": {
+            "schemas": {
+                "Invoice": invoice_schema,
+                "JSON Error": {
+                    "description": "An error occurring when the request body contains wrongly formatted JSON",
+                    "properties": {
+                        "json_error": {
+                            "description": "An error message describing the JSON error. The syntax error position is provided",
+                            "type": "string",
+                        }
+                    },
+                },
+                "Validation Error": {
+                    "description": "An error occurring when the invoice content is incorrect",
+                    "properties": {
+                        "loc": {
+                            "description": "The field sequence raising an error. `['author','name']` corresponds to `author.name`",
+                            "type": "array",
+                            "items": {"type": "string"},
+                        },
+                        "msg": {
+                            "description": "The validation error message",
+                            "type": "string",
+                        },
+                        "type": {
+                            "description": "The type of validation error",
+                            "type": "string",
+                        },
+                    },
+                },
+                "SendinBlue Error": {
+                    "description": "An error occurring when sending an email with SendinBlue service has failed",
+                    "properties": {
+                        "code": {"description": "An error code", "type": "string"},
+                        "message": {
+                            "description": "A readable message associated with the failure",
+                            "type": "string",
+                        },
+                    },
+                },
+            }
+        },
+        "definitions": definitions_schema,
+    }
+)
 
 middleware: List[Middleware] = [
     Middleware(
@@ -41,12 +96,50 @@ middleware: List[Middleware] = [
 ]
 
 app: Starlette = Starlette(debug=True, middleware=middleware)
-api.register(app)
 
 
-@api.validate(json=Invoice, tags=["api"])
 @app.route("/pdf/{name}", methods=["POST"])
 async def download_invoice(request: Request):
+    """
+    summary: Generate an invoice as PDF
+    description: Generate an invoice as PDF, based on Tarif 590 and QR-invoice standards
+
+    parameters:
+        -   in: path
+            name: name
+            schema:
+                type: string
+            required: true
+            description: The generated PDF invoice filename. It is needed when downloading the PDF from this endpoint or when opening the PDF in a browser directly from the endpoint URL. It should end with _.pdf_
+
+    requestBody:
+        summary: The content used to generate the PDF invoice
+        required: true
+        content:
+            application/json:
+                schema:
+                    $ref: '#/components/schemas/Invoice'
+
+    responses:
+        200:
+            description: The generated PDF invoice
+            content:
+                application/pdf:
+                    schema:
+                        type: string
+                        format: binary
+        400:
+            description: Bad Request Error
+            content:
+                application/json:
+                    schema:
+                        oneOf:
+                            -   $ref: '#/components/schemas/JSON Error'
+                            -   type: array
+                                items:
+                                    $ref: '#/components/schemas/Validation Error'
+    """
+
     try:
         invoice_dict: dict = await request.json()
     except JSONDecodeError as json_error:
@@ -66,9 +159,36 @@ async def download_invoice(request: Request):
     return FileResponse(invoice_path.as_posix())
 
 
-@api.validate(json=Invoice, resp=Response(HTTP_201=None, HTTP_400=None), tags=["api"])
 @app.route("/email", methods=["POST"])
 async def email_invoice(request: Request):
+    """
+    summary: Send an invoice
+    description: Generate an invoice as PDF, based on Tarif 590 and QR-invoice standards and send it to the author's and patient's mail addresses
+
+    requestBody:
+        summary: The content used to generate the PDF invoice
+        required: true
+        content:
+            application/json:
+                schema:
+                    $ref: '#/components/schemas/Invoice'
+
+    responses:
+        201:
+            description: The invoice as been successfully sent
+        400:
+            description: Bad Request Error
+            content:
+                application/json:
+                    schema:
+                        oneOf:
+                            -   $ref: '#/components/schemas/JSON Error'
+                            -   type: array
+                                items:
+                                    $ref: '#/components/schemas/Validation Error'
+                            -   $ref: '#/components/schemas/SendinBlue Error'
+    """
+
     try:
         invoice_dict: dict = await request.json()
     except JSONDecodeError as json_error:
@@ -121,9 +241,16 @@ async def email_invoice(request: Request):
     return UJSONResponse(ujson.loads(response.text), status_code=HTTP_400_BAD_REQUEST)
 
 
-@app.route("/favicon.ico")
+@app.route("/favicon.ico", include_in_schema=False)
 async def icon(_: Request):
     return RedirectResponse("https://naturapeute.ch/img/favicon.png")
+
+
+# NOTE : The schema generation is done after routes are defined so they are registered in the server
+with open("./doc/doc.yaml", "w") as doc_yaml:
+    doc_yaml.write(yaml.dump(schemas.get_schema(app.routes)))
+
+api_doc(app, config_path=Path("./doc/doc.yaml").as_posix(), url_prefix="/doc")
 
 
 def generate_invoice(invoice_content: InvoiceContent) -> Path:
